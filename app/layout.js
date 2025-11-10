50900a9399e945527b31b9796da6717be9e4297d
gaurav0973 <codejourneypanda@gmail.com>
2025-11-10 10:13:25 +0530

    co complete frontend

---
+++ app/layout.js
@@ -0,0 +1,24 @@
+import { Outfit } from "next/font/google";
+import "./globals.css";
+import { AppContextProvider } from "@/context/AppContext";
+import { Toaster } from "react-hot-toast";
+
+const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
+
+export const metadata = {
+  title: "UniTrade-AI Driven Student Marketplace",
+  description: "This is a student marketplace that leverages AI to connect buyers and sellers efficiently in college campuses.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+      <html lang="en">
+        <body className={`${outfit.className} antialiased text-gray-700`} >
+          <Toaster />
+          <AppContextProvider>
+            {children}
+          </AppContextProvider>
+        </body>
+      </html>
+  );
+}
